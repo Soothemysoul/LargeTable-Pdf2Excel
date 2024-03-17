67b0d3dc2dbb5988b93759504e05a86a745c2a3a
kolyvanov_vn <kolyvanovvn@gmail.com>
2024-03-17 17:52:57 +0300

    first commit

---
+++ CollapseFileExcel/Program.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using App = Microsoft.Office.Interop.Excel.Application;
+
+namespace CollapseFileExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string executableFile = Assembly.GetExecutingAssembly().Location;
+                DirectoryInfo directoryInfo = new DirectoryInfo(executableFile);
+
+                Console.WriteLine("Введите путь до папки с файлами: \n");
+                string folderPath = Console.ReadLine().Trim('\"');
+                Console.WriteLine();
+
+                if (folderPath == "")
+                {
+                    Console.WriteLine("Ничего не введено!\n");
+                }
+
+                string[] files = Directory.GetFiles(folderPath, "*.xlsx");
+
+                List<string>sortedPath = files.ToList();
+
+                sortedPath = sortedPath.OrderBy(p => int.Parse(Path.GetFileNameWithoutExtension(p))).ToList();
+
+                if (files.Count() == 0)
+                {
+                    Console.WriteLine("Папка пуста!\n");
+                }
+                else
+                {
+                    try
+                    {
+                        App app = new App();
+
+                        Workbook wb = app.Workbooks.Add();
+
+                        Worksheet ws = wb.Worksheets[1];
+
+                        foreach (string file in sortedPath)
+                        {
+                            Workbook partWb = app.Workbooks.Open(file);
+
+                            Console.WriteLine($"Обработка файла '{Path.GetFileName(file)}'");
+                            Console.WriteLine();
+
+                            AddedRange(partWb, ws);
+
+                            partWb.Close(false);
+
+                            File.Delete(file);
+
+                        }
+
+                        wb.SaveAs(folderPath + "\\collapsed.xlsx");
+                        wb.Close();
+                        
+
+                    }
+                    catch { }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Готово!\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
+
+        }
+
+        private static void AddedRange(Workbook partWb, Worksheet ws)
+        {
+            long lastRow = ws.Cells.SpecialCells(XlCellType.xlCellTypeLastCell).Row;
+
+            Range targetRange = ws.Range["A" + (lastRow+1).ToString()];
+
+            Worksheet sourceWs = partWb.Worksheets[1];
+            long sourceLastRow = sourceWs.Cells.SpecialCells(XlCellType.xlCellTypeLastCell).Row;
+
+            for (long i = sourceLastRow; i > 0; i--)
+            {
+
+                bool isMerged;
+
+                if (sourceWs.Cells[i, 1].MergeCells &&
+                    sourceWs.Cells[i, 5].MergeCells &&
+                    sourceWs.Cells[i, 9].MergeCells &&
+                    sourceWs.Cells[i, 13].MergeCells &&
+                    sourceWs.Cells[i, 17].MergeCells)
+                {
+                    isMerged = true;
+                }
+                else
+                {
+                    isMerged = false;
+                }
+                
+
+                if (isMerged)
+                {
+                    Range mergedRange = sourceWs.Cells[i, 1].MergeArea;
+
+                    mergedRange.ClearContents();
+
+                    // получаем номера строк, которые попадают в объединенную ячейку
+                    int startRow = mergedRange.Row;
+                    int endRow = startRow + mergedRange.Rows.Count - 1;
+
+                    // удаляем строки
+                    sourceWs.Rows[startRow+":"+endRow].ClearContents();
+                    sourceWs.Rows[endRow].Delete();
+
+                }
+
+            }
+
+            Range usedSourceRange = sourceWs.UsedRange;
+
+            usedSourceRange.Copy(targetRange);
+
+
+        }
+    }
+}
---
+++ PdfToExcel/Program.cs
@@ -0,0 +1,160 @@
+﻿using Acrobat;
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using Path = System.IO.Path;
+
+
+namespace PdfToExcel
+{
+    internal class Program
+    {
+        public static AcroApp AcrobatApp { get; private set; }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                AcrobatApp = new AcroApp();
+
+
+                string executableFile = Assembly.GetExecutingAssembly().Location;
+                DirectoryInfo directoryInfo = new DirectoryInfo(executableFile);
+
+                Console.WriteLine("Введите путь до pdf файла для экспорта: \n");
+                string filePath = Console.ReadLine().Trim('\"');
+                Console.WriteLine();
+
+                if (filePath == "")
+                {
+                    Console.WriteLine("Ничего не введено!\n");
+                }
+
+                bool fileExist = File.Exists(filePath);
+                if (!fileExist)
+                {
+                    Console.WriteLine("Файла не существует!\n");
+                }
+                else
+                {
+                    try
+                    {
+
+                        Console.WriteLine($"Обработка файла '{filePath}'");
+                        Console.WriteLine();
+                        Export(filePath);
+
+
+                    }
+                    catch { }
+
+
+                }
+
+                AcrobatApp.Maximize(1);
+                AcrobatApp.Exit();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Готово!\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
+
+        }
+
+        private static void Export(string pdfFile)
+        {
+
+            string tmpPdf = pdfFile.Replace(Path.GetFileName(pdfFile), $"tmp.pdf");
+
+            int pagesRangeCount = 300;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(pdfFile);
+
+            CAcroAVDoc doc = new AcroAVDoc();
+
+            int pagesCount = 0;
+
+            if (doc.Open(pdfFile, null))
+            {
+                CAcroPDDoc pdfDoc = doc.GetPDDoc();
+                pagesCount = pdfDoc.GetNumPages();
+
+            }
+
+            doc.Close(0);
+
+            int hundreedsPage = pagesCount / pagesRangeCount;
+            int balancePage = pagesCount % pagesRangeCount;
+
+            for (int i = 0; i <= hundreedsPage; i++)
+            {
+                Console.WriteLine($"Итерация {i + 1}\n");
+
+                string tmpExcel = pdfFile.Replace(Path.GetFileName(pdfFile), $"{i + 1}.xlsx");
+
+                if (doc.Open(pdfFile, null))
+                {
+
+                    CAcroPDDoc pdfDoc = doc.GetPDDoc();
+
+                    int[] pageRange = new int[2];
+                    pageRange[0] = i * pagesRangeCount;
+
+                    if (i * pagesRangeCount > pagesCount)
+                    {
+                        pageRange[1] = pagesCount - 1;
+                    }
+                    else
+                    {
+                        pageRange[1] = pagesRangeCount + i * pagesRangeCount;
+                    }
+
+                    Console.WriteLine($"Страниц до обрезки: {pdfDoc.GetNumPages()}\n");
+
+                    if (pageRange[0] == 0)
+                    {
+                        pdfDoc.DeletePages(pageRange[1], pdfDoc.GetNumPages() - 1);
+                    }
+                    else if (pageRange[1] == pagesCount - 1)
+                    {
+                        pdfDoc.DeletePages(0, pageRange[0] - 1);
+                    }
+                    else
+                    {
+                        pdfDoc.DeletePages(pageRange[1], pdfDoc.GetNumPages() - 1);
+                        pdfDoc.DeletePages(0, pageRange[0] - 1);
+                    }
+
+                    Console.WriteLine($"Страниц после обрезки: {pdfDoc.GetNumPages()}\n");
+
+                    pdfDoc.Save(1, tmpPdf);
+                    Object jsoObj = pdfDoc.GetJSObject();
+
+                    Type jsType = jsoObj.GetType();
+                    //have to use acrobat javascript api because, acrobat
+                    object[] saveAsParam = { tmpExcel, "com.adobe.acrobat.xlsx", "", false, false};
+                    jsType.InvokeMember("saveAs", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, jsoObj, saveAsParam, CultureInfo.InvariantCulture);
+
+                    pdfDoc.Close();
+
+
+                }
+
+                doc.Close(0);
+                File.Delete(tmpPdf);
+            }
+
+
+
+        }
+    }
+}
+
